8c898b8b6b930bf9ebd8af3a15d41650fdfe95f5
Administracion <Administracion@grupopcr.com.pa>
2025-04-22 21:13:56 -0500

    desde el 6

--- src/app/controllers/RegcolaController.php
+++ src/app/controllers/RegcolaController.php
@@ -50,7 +50,7 @@ if (isset($_GET['restablecer_password'])) {
 
         $userModel->actualizar_colaborador($_POST['new_pass2'], $_POST['restore_code']);
         $mensaje = 'Contraseña actualizada';
-        header("Location: /../views/login.php?msg=Contraseña+actualizada+correctamente");
+        header("Location: https://apppcr.net/index.php?msg=Contraseña+actualizada+correctamente");
         //require_once __DIR__ . '/../views/login.php';
         exit();
         
