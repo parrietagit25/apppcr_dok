49ea4d78ba527fd7b97a34924aca259c450f300c
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-04-23 21:51:47 -0500

    despues de la restauracion

--- src/app/views/login.php
+++ src/app/views/login.php
@@ -1,3 +1,13 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (isset($_SESSION['code'])) {
+    header("Location: main.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
