3fa07e4248fce3dde84fffa184a0c0434a5d89b2
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-05-02 11:59:34 -0500

    cambios

--- src/app/models/Rrhh.php
+++ src/app/models/Rrhh.php
@@ -241,7 +241,7 @@ class Rrhh {
                                             WHEN 2 THEN 'Revisado'
                                         END AS estado, 
                                         c.nombre,
-                                        ct.file_add FROM calamidades ct inner join col_datos_generales c on ct.code_user = c.codigo  WHERE ct.stat in(1,2)");
+                                        ct.file_add FROM calamidades ct inner join col_datos_generales c on ct.code_user = c.codigo  WHERE ct.stat in(0)");
         $stmt->execute();
         $array_datos = [];
         while ($list_code = $stmt->fetch(PDO::FETCH_ASSOC)) {
