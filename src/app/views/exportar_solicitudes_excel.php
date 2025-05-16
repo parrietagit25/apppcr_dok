908211e7bc0614b7bd11e440b8db5fac5e95475e
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-05-15 19:49:34 -0500

    exportar a excel

--- src/app/views/exportar_solicitudes_excel.php
+++ src/app/views/exportar_solicitudes_excel.php
@@ -1,5 +1,5 @@
 <?php
-require '../vendor/autoload.php';
+require '../../vendor/autoload.php';
 require_once '../models/Rrhh.php'; // Asegúrate de usar el modelo correcto
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
