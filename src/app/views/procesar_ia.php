860eb81b477fa5497799c305ffd6c7902d3cc146
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-05-29 23:16:52 -0500

    ia config

--- src/app/views/procesar_ia.php
+++ src/app/views/procesar_ia.php
@@ -94,7 +94,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pregunta'])) {
                 foreach ($resultados as $fila) {
                     $html .= "<tr>";
                     foreach ($fila as $valor) {
-                        $html .= "<td>" . htmlspecialchars($valor) . "</td>";
+                        $html .= "<td>" . htmlspecialchars($variable ?? '', ENT_QUOTES, 'UTF-8') . "</td>";
                     }
                     $html .= "</tr>";
                 }
