fa600d9af65c6da338a431d15bf282d6dc98082a
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-06-08 20:24:24 -0500

    cambiar stilos

--- src/app/views/header.php
+++ src/app/views/header.php
@@ -178,7 +178,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="<?php echo BASE_URL_IMAGE; ?>logotop.png" alt="" srcset=""></a>
+            <a class="navbar-brand" href="#"><img width="200" src="<?php echo BASE_URL_IMAGE; ?>logotop.png" alt="" srcset=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
