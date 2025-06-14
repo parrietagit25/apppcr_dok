fd44910728492a3114f33a6ec409df77b9feb121
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-06-14 14:02:41 -0500

    qr

--- src/app/views/generar_qr.php
+++ src/app/views/generar_qr.php
@@ -2,14 +2,22 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Endroid\QrCode\QrCode;
-use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\LabelAlignment;
+use Endroid\QrCode\Response\QrCodeResponse;
 
 $codigo = $_GET['codigo'] ?? 'sin-codigo';
 $contenido = "https://apppcr.net/validar_empleado.php?codigo=" . urlencode($codigo);
 
+// Crear QR
 $qrCode = new QrCode($contenido);
-$qrCode->setSize(200);
+$qrCode->setSize(300);
+$qrCode->setMargin(10);
+$qrCode->setEncoding('UTF-8');
+$qrCode->setErrorCorrectionLevel(new ErrorCorrectionLevel(ErrorCorrectionLevel::HIGH));
+$qrCode->setRoundBlockSize(true);
+$qrCode->setValidateResult(false);
 
-header('Content-Type: image/png');
-$writer = new PngWriter();
-echo $writer->write($qrCode)->getString();
+// Mostrar QR como imagen
+header('Content-Type: '.$qrCode->getContentType());
+echo $qrCode->writeString();
