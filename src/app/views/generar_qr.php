a275e058e66554c7469544e94e8e742e88119747
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-06-14 12:50:21 -0500

    qr

--- src/app/views/generar_qr.php
+++ src/app/views/generar_qr.php
@@ -2,12 +2,7 @@
 require_once __DIR__ . '/../libs/phpqrcode/qrlib.php';
 
 $codigo = $_GET['codigo'] ?? 'sin-codigo';
-
-// Puedes usar contenido simple o una URL personalizada:
 $contenido = "https://apppcr.net/validar_empleado.php?codigo=" . urlencode($codigo);
 
-// Encabezado para indicar que se devuelve una imagen
 header('Content-Type: image/png');
-
-// Generar QR directamente en la salida
 QRcode::png($contenido, false, QR_ECLEVEL_L, 4);
