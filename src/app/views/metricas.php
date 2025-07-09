355a80f47dcb24cf1d032f90e18084c648db2463
Grupo PCR <76816677+TecnologiaPCR@users.noreply.github.com>
2025-07-08 20:15:39 -0500

    metricas

--- src/app/views/metricas.php
+++ src/app/views/metricas.php
@@ -31,9 +31,7 @@ include __DIR__ . '/header.php';
             </div>
             <div class="col-4 mb-3">
                 <a href="#" class="text-decoration-none">
-                     <?php foreach ($cartas_trabajos as $key => $value): ?> 
-                    <h2><?php echo $value['total']; ?></h2>
-                    <?php endforeach; ?>
+                    <h2><?php echo $cartas_trabajos[0]['total']; ?></h2>
                     <div class="text-muted small">Solicitud de Carta de trabajo</div>
                 </a>
             </div>
