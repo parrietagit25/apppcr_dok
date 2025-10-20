c511b482c2cb5f912378e3a99c68093342d2f2d8
Pedro Arrieta Perez <pedro.arrieta@ad.grupopcr.com.pa>
2025-10-20 11:23:31 -0500

    uniformes

--- src/app/controllers/RRHHController.php
+++ src/app/controllers/RRHHController.php
@@ -1126,10 +1126,10 @@ if (isset($_GET['mis_datos']) && $_GET['mis_datos'] == 1) {
                                 <b>Observaciones:</b> $observacion<br><br>
                                 Por favor, revise la solicitud en el sistema.";
                     
-                    $copia = ["abi.pineda@grupopcr.com.pa", "yissell.perez@grupopcr.com.pa", "pedro.arrieta@grupopcr.com.pa"];
+                    $copia = ["abi.pineda@grupopcr.com.pa", "yissell.perez@grupopcr.com.pa"];
                     $class->enviar_correo("sofia.macias@grupopcr.com.pa", $copia, "Nueva solicitud de uniforme", $mensaje);
                 }
-                
+
             } else {
                 echo "<div class='alert alert-danger'>Error al procesar la solicitud.</div>";
             }
