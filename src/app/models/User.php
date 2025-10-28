32d47b3bc7f5e436a770c0b3c3e9c85e8fa6834a
Pedro Arrieta Perez <pedro.arrieta@ad.grupopcr.com.pa>
2025-10-28 11:53:43 -0500

    permisos de los jefes

--- src/app/models/User.php
+++ src/app/models/User.php
@@ -185,7 +185,7 @@ public function nombre_colaborador() {
     }
 
     public function usuarios_encargados() {
-        $stmt = $this->pdo->prepare("SELECT * FROM empleado_log el INNER JOIN empleados e ON el.codigo = e.codigo_empleado WHERE el.stat = 1 AND e.type_user = 6");
+        $stmt = $this->pdo->prepare("SELECT * FROM empleado_log el INNER JOIN empleados e ON el.codigo = e.codigo_empleado WHERE el.stat = 1 AND el.type_user = 6");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
