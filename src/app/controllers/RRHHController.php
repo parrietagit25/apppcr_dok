1ee4c593aeede563a68256795a3aa72b2017bdff
Pedro Arrieta Perez <pedro.arrieta@ad.grupopcr.com.pa>
2025-10-28 13:23:24 -0500

    permisos de los jefes

--- src/app/controllers/RRHHController.php
+++ src/app/controllers/RRHHController.php
@@ -817,9 +817,13 @@ if (isset($_GET['mis_datos']) && $_GET['mis_datos'] == 1) {
 
         <p><strong>Descripción del permiso:</strong><br>' . nl2br($descripcion) . '</p>
 
-        <h4 style="color:rgb(250, 11, 2);">Para aprobar o declinar esta solicitud, ingrese en el siguiente link.</h4>
+        <h4 style="color:rgb(250, 11, 2);">Para aprobar o declinar esta solicitud, ingrese al app Gente PCR. En la opcion de Administrar Permisos podra ver la solicitud y aprobarla o declinarla.</h4>
+        ';
 
-        <p><a href="https://apppcr.net/app/views/aprobar_vacaciones.php?codigo_empleado=' . $codigo . '&nombre_completo=' . $nombre_comple . '&fecha_desde='.$fecha_inicio.'&fecha_hasta='.$fecha_fin.'&cantidad_dias='.$cantidad_dias.'">Aprobar o Declinar Solicitud de Permiso</a></p>
+        //<p><a href="https://apppcr.net/app/views/aprobar_vacaciones.php?codigo_empleado=' . $codigo . '&nombre_completo=' . $nombre_comple . '&fecha_desde='.$fecha_inicio.'&fecha_hasta='.$fecha_fin.'&cantidad_dias='.$cantidad_dias.'">Aprobar o Declinar Solicitud de Permiso</a></p>
+        $mensaje .= '
+
+        <p><a href="https://apppcr.net/>Gente PCR</a></p>
 
         <p><strong>Canal de contacto:</strong></p>
         <ul>
