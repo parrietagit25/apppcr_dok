061174fe88726f344c949d1aca927bd4e47c210c
Pedro Arrieta Perez <pedro.arrieta@ad.grupopcr.com.pa>
2026-05-30 14:08:17 -0500

    nuevo seguro

--- src/app/views/n_poliza.php
+++ src/app/views/n_poliza.php
@@ -26,7 +26,7 @@ include __DIR__ . '/header.php';
             </a>
         </div>
         <div class="col-4 mb-3">
-            <a href="<?php echo BASE_URL_CONTROLLER; ?>/MainController.php?portal_asegurados=1" class="text-decoration-none">
+            <a href="https://attest.palig.com/as/authorization.oauth2?response_type=code&amp;client_id=9effd84c2b0a471b9f1d2a4dfd4114c8&amp;redirect_uri=https%3A%2F%2Fpalig.my.site.com%2Fmembersportal%2Fservices%2Fauthcallback%2FMemberPortal&amp;scope=openid&amp;state=CAAAAZ51fKOnMDAwMDAwMDAwMDAwMDAwAAABBBfRJX3fWRF6JQJ892zWcA8Sj8rlGxGIf3b4k8Xo00OMfIy3a9d4XJ7RW-zZV8YiIQ9kGBAMCW1cYZfyMt3xWXz6jiWcGfThDhQtX8d5ymt8DkSjQnTTb-84rBuDToNX8gkPH6vliYJT3A8wfXBVvD7ftCGo9ai2NnRaW84s3GMFUNC1n_oEm5xzdpHSYljqGqfbX8GrbkEb5yMLeK0Z9Gsn9LA2BcllbmIPuhC1xW4Z78f80qBnibKjgiFo5irXevIuUo6yuYPjdP63C7p0BvE_PbMFcXtb3iNP7pcp1s5-" class="text-decoration-none" target="_blank" rel="noopener noreferrer">
                 <div class="card-icon">
                     <img src="<?php echo BASE_URL_IMAGE; ?>portal_asegurados.svg" alt="Portal de asegurados">
                     <div class="card-title"><small>Portal de asegurados</small></div>
